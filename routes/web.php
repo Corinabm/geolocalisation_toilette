81367d93d2629baf26cc54b206fc26449f6dc4b4
Corinabm <bellemarecorina@gmail.com>
2025-08-27 11:52:42 -0400

    route ToiletteController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToiletteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/toilettes',[ToiletteController::class, 'index']);
