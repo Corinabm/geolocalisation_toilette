5ad94741795293b6133202bb4422248e7b3668d7
Corinabm <bellemarecorina@gmail.com>
2025-08-28 21:14:25 -0400

    mise à jour du controller

--- app/Http/Controllers/ToiletteController.php
+++ app/Http/Controllers/ToiletteController.php
@@ -9,8 +9,25 @@ class ToiletteController extends Controller
 {
     public function index()
     {
-        $toilettes = Toilette::all(); 
-        dd($toilettes); 
+        /*$toilettes = Toilette::all(); 
+        dd($toilettes); */
+
+        /* retourne la vue Blade qui contient la map */
+        return view('toilettes.index');
+
+    }
+
+    /**
+     * Fournit les données des toilettes sous forme d'API JSON.
+     */
+    public function api(Request $request)
+    {
+        // Récupère toutes les toilettes avec leurs localisations associées.
+        // On utilise la méthode 'with()' pour éviter le problème du N+1 (chargement des relations en une seule requête).
+        $toilettes = Toilette::with('localisation')->get();
+
+        // Retourne les données au format JSON.
+        return response()->json($toilettes);
     }
 
 }
