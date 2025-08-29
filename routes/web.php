512f2e52832fb30323553363c7e215d1229ed112
Corinabm <bellemarecorina@gmail.com>
2025-08-28 21:10:29 -0400

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToiletteController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/toilettes',[ToiletteController::class, 'index']);
+Route::get('/toilettes',[ToiletteController::class, 'index']);*/
+
+/* affiche la page principale */
+Route::get('/',[ToiletteController::class, 'index'])->name('toilettes.index');
+
+/* fournir les données des toilettes au format json */
+Route::get('/api/toilettes', [ToiletteController::class, 'api'])->name('toilettes.api');
